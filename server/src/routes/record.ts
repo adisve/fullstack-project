88c36afc2a9f29ea7cb473ae189d8dcfffaa2f6f
Adis Veletanlic <adis.veletanlic@gmail.com>
2023-03-30 00:02:55 +0200

    Add typescript configurations in ./server. Edit Dockerfile in ./server (only for dev builds)

---
+++ server/src/routes/record.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { getDb } from '../db/connection';
+
+const recordRoutes = express.Router();
+
+/**
+ * Example route to display how to connect to a collecton,
+ * find an item in the collection
+ */
+recordRoutes.route('/record').get((req: Request, res: Response) => {
+  const db_connect = getDb();
+  db_connect
+    .collection('records')
+    .find({});
+});
+
+export default recordRoutes;
