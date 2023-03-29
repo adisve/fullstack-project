0c9483e01004f140820c3e88888016362667c74f
PsychicPlatypus <dzenismadzovic@gmail.com>
2023-03-29 19:05:38 +0200

    Added mui, created a components+ css example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
-import { LandingPage } from './components/LandingPage';
+import { LandingPage } from './components/landingPage/LandingPage';
 import { Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <main>
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                </Routes>
-            </main>
+            <Container>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                    </Routes>
+                </main>
+            </Container>
         </div>
     );
 }
