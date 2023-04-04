6fa553cad03bc1bd3967b8b03af5daa1e6b78be4
PsychicPlatypus <dzenismadzovic@gmail.com>
2023-04-05 01:39:58 +0200

    Added fa, changed logo to icon

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import { AppBar, Stack, Toolbar, Link } from '@mui/material';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 export function Footer() {
     return (
         <AppBar position="static" color="transparent" id="footer">
             <Stack direction={'row'} spacing={3}>
                 <Toolbar>
-                    <h3>Logo</h3>
+                    <h3>ProTracker</h3> {'\u00A0'}
+                    <FontAwesomeIcon icon={faDumbbell} />
                 </Toolbar>
                 <Toolbar disableGutters={true}>
                     <Stack direction={'row'} spacing={3}>
--- client/src/components/landingPage/NavBar.tsx
+++ client/src/components/landingPage/NavBar.tsx
@@ -1,11 +1,14 @@
-import { AppBar, Container, Toolbar, Box, Stack, Link } from '@mui/material';
+import { AppBar, Toolbar, Stack, Link } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 
 export function NavBar() {
     return (
         <AppBar position="static" color="transparent" id="navbar">
             <Stack direction={'row'} id="navbar-container">
                 <Toolbar disableGutters>
-                    <h3>Logo</h3>
+                    <h3>ProTracker</h3> {'\u00A0'}
+                    <FontAwesomeIcon icon={faDumbbell} />
                 </Toolbar>
                 <Toolbar disableGutters>
                     <Stack direction={'row'} spacing={3}>
