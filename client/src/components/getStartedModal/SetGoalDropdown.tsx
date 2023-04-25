73bdc3d0e2ff326e78bc9623b4b92d09777e98ba
Liis U <liis.usin@gmail.com>
2023-04-25 16:49:40 +0200

    set workout goal component

---
+++ client/src/components/getStartedModal/SetGoalDropdown.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import {
+    InputLabel,
+    MenuItem,
+    FormControl,
+    SelectChangeEvent,
+    Select,
+} from '@mui/material';
+import workoutGoals from '../../assets/resources/workoutGoals.json';
+
+export function SetGoalDropdown() {
+    const [goal, setGoal] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        console.log(event.target.value);
+        setGoal(event.target.value);
+    };
+    return (
+        <div>
+            <p>What is your goal?</p>
+            <div className="modalContentCenter">
+                <FormControl className="goalForm">
+                    <InputLabel id="demo-simple-select-autowidth-label">
+                        Goal
+                    </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-autowidth-label"
+                        id="demo-simple-select-autowidth"
+                        value={goal}
+                        onChange={handleChange}
+                        autoWidth
+                        label="Goal"
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+
+                        {workoutGoals.goals.map((goal) => (
+                            <MenuItem key={goal} value={goal}>
+                                {goal}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </div>
+        </div>
+    );
+}
