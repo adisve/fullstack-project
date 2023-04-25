86b2fbeb28683b1dfb31727adea8ac68ec5f720d
Liis U <liis.usin@gmail.com>
2023-04-25 16:46:39 +0200

    Choose workout component

---
+++ client/src/components/getStartedModal/ChooseWorkoutToggle.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import workoutRoutines from '../../assets/resources/workouts.json';
+
+export function ChooseWorkoutToggle() {
+    const [exercise, setExercise] = useState<string[]>();
+
+    const handleExercise = (
+        event: React.MouseEvent<HTMLElement>,
+        newExercise: string[]
+    ) => {
+        console.log(newExercise);
+        setExercise(newExercise);
+    };
+
+    return (
+        <>
+            <p>What are you interested in?</p>
+            <div className="modalContentFlexCenter">
+                <ToggleButtonGroup
+                    className="chooseWorkoutButtonGroup"
+                    value={exercise}
+                    onChange={handleExercise}
+                    aria-label="text formatting"
+                >
+                    {workoutRoutines.map(({ workoutCategory }) => (
+                        <ToggleButton
+                            key={workoutCategory}
+                            className="chooseWorkoutButton"
+                            value={workoutCategory}
+                            aria-label="bold"
+                            sx={{
+                                '&.MuiToggleButtonGroup-grouped': {
+                                    borderRadius: '10px !important',
+                                    m: 0.5,
+                                },
+                            }}
+                        >
+                            <div className="workouts">{workoutCategory}</div>
+                        </ToggleButton>
+                    ))}
+                </ToggleButtonGroup>
+            </div>
+        </>
+    );
+}
