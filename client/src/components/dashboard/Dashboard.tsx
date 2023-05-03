5b536fd26a6165d17dc6abdf3bab5c5eceac580e
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-05-03 18:26:21 +0200

    Created sidebar, added button to cards

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Container, Fab } from '@mui/material';
+import { Box, Container, Drawer, Fab } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import { ActionCard } from './ActionCard';
+import { ActionCard } from './actionCard/ActionCard';
 import UserHeader from './userHeader/UserHeader';
+import { useEffect, useState } from 'react';
+import { Sidebar } from './sidebar/Sidebar';
 
 export function Dashboard() {
+    const [activeState, setActiveState] = useState(false);
+
     return (
         <Container
             sx={{
@@ -15,23 +19,20 @@ export function Dashboard() {
                 justifyContent: 'center',
             }}
         >
-            <UserHeader />
-            <Outlet />
-            <ActionCard
-                actionName="Browse workouts"
-                actionDescription="Look through a collection of workouts, tailored to your profile."
+            <UserHeader
+                toggleDrawer={setActiveState}
+                drawerState={activeState}
             />
-            <ActionCard
-                actionName="Making your own workouts"
-                actionDescription="Be creative and plan your own workout or make one from a provided template."
-            />
-            <ActionCard
-                actionName="Nutritional plans"
-                actionDescription="Check out our nutritional programs and plans that could offer you extra opportunities during your training process."
-            />
-            <ActionCard
-                actionName="Your progress"
-                actionDescription="View your progress, receive analytical feedback, and see how far you've come."
+            <Box>
+                <Outlet />
+                <ActionCard
+                    actionName="Browse workouts"
+                    actionDescription="Look through a collection of workouts, tailored to your profile."
+                />
+            </Box>
+            <Sidebar
+                activeState={activeState}
+                toggleActiveState={setActiveState}
             />
             <Fab className="floating-btn" variant="extended">
                 <FontAwesomeIcon
