53784a5ecf140503f6e5f08165228f7d20fd65a5
adisve <adis.veletanlic@gmail.com>
2023-05-07 16:16:53 +0200

    Intercept all routes requireing auth from /homepage/*

--- client/src/components/homepage/HomePage.tsx
+++ client/src/components/homepage/HomePage.tsx
@@ -4,9 +4,18 @@ import { Outlet } from 'react-router-dom';
 import UserHeader from './userHeader/UserHeader';
 import { useState } from 'react';
 import { Sidebar } from './sidebar/Sidebar';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { AuthenticationModal } from '../dashboard/login-register/AuthenticationModal';
 
 export function HomePage() {
     const [activeState, setActiveState] = useState(false);
+
+    const token = useSelector((state: RootState) => state.auth.token);
+
+    if (!token) {
+        return <AuthenticationModal open={token == undefined} />;
+    }
 
     return (
         <>
--- client/src/components/homepage/dashboard/Dashboard.tsx
+++ client/src/components/homepage/dashboard/Dashboard.tsx
@@ -3,19 +3,10 @@ import { ActionCard } from './actionCard/ActionCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { AuthenticationModal } from '../../dashboard/login-register/AuthenticationModal';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/store';
 
 export function Dashboard() {
-    const [activeState, setActiveState] = useState(false);
-
-    const token = useSelector((state: RootState) => state.auth.token);
-
-    if (!token) {
-        return <AuthenticationModal open={token == undefined} />;
-    }
-
     return (
         <Container>
             <Box>
