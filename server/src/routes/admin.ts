63f12ab7295f7b54d9ae4a7d2949482acbc0e592
Liis U <liis.usin@gmail.com>
2023-05-09 15:43:17 +0200

    admin delete user

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -7,9 +7,20 @@ router.get('/', async (req: Request, res: Response) => {
     res.json({ message: 'admin route' });
 });
 
-router.get('/allUsers', async (req: Request, res: Response) => {
+router.get('/users', async (req: Request, res: Response) => {
     const users = await User.find();
     res.json(users);
+});
+
+router.delete('/user/:id', async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message: 'Successfully deleted' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Failed to delete user' });
+    }
 });
 
 export default router;
