4ddb32690c0302f0ab7aea1e0ed4b167b1d1a22f
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-05-14 23:45:19 +0200

    get back the auth stuff

--- client/src/components/homepage/HomePage.tsx
+++ client/src/components/homepage/HomePage.tsx
@@ -16,16 +16,16 @@ export function HomePage() {
     const [activeState, setActiveState] = useState(false);
     const authStatus = useSelector((state: RootState) => state.auth.status);
 
-    // if (
-    //     authStatus == AuthStatus.unauthenticated ||
-    //     authStatus == AuthStatus.error
-    // ) {
-    //     return <AuthenticationModal open={true} />;
-    // }
+    if (
+        authStatus == AuthStatus.unauthenticated ||
+        authStatus == AuthStatus.error
+    ) {
+        return <AuthenticationModal open={true} />;
+    }
 
-    // if (authStatus == AuthStatus.loading) {
-    //     return <LoadingSpinner />;
-    // }
+    if (authStatus == AuthStatus.loading) {
+        return <LoadingSpinner />;
+    }
 
     return (
         <>
