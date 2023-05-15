926f61aa8c207f0d5d46c6afe450bcef3b19f5cf
Adis Veletanlic <96535657+adisve@users.noreply.github.com>
2023-05-16 00:52:06 +0200

    Edit env location in all files

--- server/src/routes/exercises.ts
+++ server/src/routes/exercises.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Router } from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: '/etc/secrets/config.env' });
 const bodyParser = require('body-parser');
 import { getMatchedExercises } from '../utils/exerciseRecommendations';
 import { createExercises } from '../utils/generatingExercises';
